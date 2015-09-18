ae69199b4d39a4ac25fb54ed1ddc17ae0bdb2e71
Tessa <Tessachi33@yahoo.com>
2015-09-18 10:55:19 -0700

    Try recursive, not passing first test

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
+var getBottles = prompt("So, you wanna drink absinthe? " + " How may bottles?!");
+var getBottlesArray = [];
+for(var i = amount; i > 0; i--){
+  getBottlesArray.push(amount + " bottles of absinthe on the wall" + amount +
+     " bottles of absinthe,take a slug and pass is down " + (amount -1) + " bottles of absinthe " );
+
+
+
+
+
+
 
 
 $(document).ready(function() {
   $("form#bottles").submit(function() {
     var bottleAmount = parseInt($("input#amount").val());
-    
     $(".results").show();
     event.preventDefault();
   });
